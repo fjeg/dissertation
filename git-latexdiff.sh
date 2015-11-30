f4cb87de56a3a9cec2a20363a97be987c9c9e74e
Francisco Gimenez <fgimenez@stanford.edu>
2015-11-30 04:49:36 -0800

    fixed latexdiff script to properly include other text and temp files

--- git-latexdiff.sh
+++ git-latexdiff.sh
@@ -1,20 +1,42 @@
 #!/bin/bash
+#set -x
 
-# checkout master version of dissertaiton into "old.tex"
-git show master:fgimenez_dissertation.tex > tmp_old.tex
+# m = filename
+# dir = full path to dissertation directory
+# ver = branch
+#
+#m=$1
+#dir=$2
+#ver=$3
 
-# expand comparison files so we don't generate annoying aux files
-latexpand fgimenez_dissertation.tex > tmp_new_expanded.tex
-latexpand tmp_old.tex > tmp_old_expanded.tex
+m="fgimenez_dissertation.tex"
+dir="/Users/fgimenez/Documents/Stanford/projects/dissertation"
+ver="master"
+dir_old=`mktemp -dt git-latexdiff`
 
-# diff the versions
-latexdiff tmp_old_expanded.tex tmp_new_expanded.tex > diff.tex
+#copy the git repo
+echo "Copying current directory to $dir_old"
+cp -R "$dir/" "$dir_old"
+
+#go into the old version and checkout desired version
+echo "Checking out old branch"
+cd "$dir_old"
+git checkout -f "$ver" .
+latexpand "$m" > "old.tex"
+cd -
+
+#diff the two repos
+echo "Diffing files"
+latexpand "$dir/$m" > "$dir/curr.tex"
+latexdiff --config="PICTUREENV=(?:picture|DIFnomarkup|description|table|sidewaystable)[\w\d*@]*" "$dir_old/old.tex"  "$dir/curr.tex" > "$dir/diff.tex"
 
 # compile the diff
-latexmk -pdf -outdir=diff diff.tex
+echo "Compiling diff"
+mkdir -p "$dir/diff"
+latexmk -pdf -outdir="$dir/diff" diff.tex
 
 # move diff file to the diff directory
-mv diff.tex diff/
-
-# delete all tmp files
-rm tmp*
+echo "Cleaning up"
+mv "$dir/diff.tex" "$dir/diff/"
+rm -f "$dir/curr.tex"
+rm -rf "$dir_old"
